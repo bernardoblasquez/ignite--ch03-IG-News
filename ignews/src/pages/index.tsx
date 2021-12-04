06fa9e8e66e2c5a12945eb8a45e842cc76defd85
Bernardo <bernardo.bt@gmail.com>
2021-12-04 08:41:54 -0300

    feat: criação de estrutura e aplicação de classe na Home

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,6 +1,23 @@
+import Head from 'next/head';
+import styles from './home.module.scss';
 
 export default function Home() {
-  return (
-    <h1>Hellowww</h1>
-  )
+	return (
+		<>
+			<head>
+				<title>home</title>
+			</head>
+			<main className={styles.contentContainer}>
+				<section className={styles.hero}>
+					<span>Hey, Welcome</span>
+					<h1>News about the <span>React</span> World</h1>
+					<p>
+						Get access to all the publications<br />
+						<span>for 19,90 month</span>
+					</p>
+				</section>
+				<img src="/images/avatar.svg" alt="Girl Coding" />
+			</main> 
+		</>
+	)
 }
