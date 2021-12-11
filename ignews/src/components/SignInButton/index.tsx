6014b67b1727697eafc0b5b577c43c0a3b9f1a1c
Bernardo <bernardo.bt@gmail.com>
2021-12-11 11:04:23 -0300

    feat: uso das funções de sessão do next-auth

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,27 +1,33 @@
 import {FaGithub} from "react-icons/fa";
-import {FiX} from "react-icons/fi"
+import {FiX} from "react-icons/fi";
+import { signIn, useSession, signOut } from "next-auth/react"
 import styles from './style.module.scss';
 
 export function SignInButton() {
-   const isUserLoggedIn = true;
+   const {data:session} = useSession();
 
    return(
 
-      isUserLoggedIn 
+      session 
       ? (
          <button
             type="button"
             className={styles.signInButton}
          >
             <FaGithub color="#04d361" />
-            Bernardo Blasquez
-            <FiX color="#737380" className={styles.closeIcon} />
+               {session.user.name}
+            <FiX 
+               color="#737380" 
+               className={styles.closeIcon} 
+               onClick={()=>signOut()}
+            />
          </button>
       )
       : (
          <button
             type="button"
             className={styles.signInButton}
+            onClick={()=>signIn('github')}
          >
             <FaGithub color="#eba417" />
             Sign in with Github
