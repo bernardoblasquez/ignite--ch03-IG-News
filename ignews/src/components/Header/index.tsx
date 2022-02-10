e61edc3ea093aac0bfb670d63b8a19b6c81776df
Bernardo <bernardo.bt@gmail.com>
2022-02-10 08:48:00 -0300

    feat: criação e uso do componente ActiveLink.

--- ignews/src/components/Header/index.tsx
+++ ignews/src/components/Header/index.tsx
@@ -1,5 +1,7 @@
- import {SignInButton} from "../SignInButton"
- import styles from './styles.module.scss';
+import Link from 'next/link';
+import {SignInButton} from '../SignInButton';
+import styles from './styles.module.scss';
+import {ActiveLink} from '../ActiveLink';
 
 export function Header(){
    return(
@@ -7,8 +9,12 @@ export function Header(){
          <div className={styles.headerContent}>
             <img src="/images/logo.svg" alt="ig.news" />
             <nav>
-               <a className={styles.active}>Home</a>
-               <a href="">Posts</a>
+               <ActiveLink href="/" activeClassName={styles.active}>
+                  <a>Home</a>
+               </ActiveLink>
+               <ActiveLink href="/posts" activeClassName={styles.active} prefetch>
+                  <a>Posts</a>
+               </ActiveLink>
             </nav>
 
             <SignInButton />
